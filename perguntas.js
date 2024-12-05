266683ca73e08cdd32a352b1d47fb15f2499a9b3
JveniJoestar <00001102351477SP@al.educacao.sp.gov.br>
2024-12-05 12:06:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Tipo fogo',
+    'Qual as vantagens e fraquezas do tipo fogo?',
+    'Possuem vantagens contra metal, inseto, gelo e planta, mas possuem desvantagens contra água, terra e pedra'
+)
+
+criaCartao(
+    'Tipo água',
+    'Qual as vantagens e fraquezas do tipo água?',
+    'Possuem vantagens contra tipo terra, fogo e pedra, mas possuem desvantagens contra planta e elétrico'
+)
+
+criaCartao(
+    'Tipo Gelo',
+    'Qual as vantagens e fraquezas do tipo gelo?',
+    'Possuem vantagens contra Dragão, voador, terra e planta, mas possuem desvantagens contra fogo, metal, pedra e lutador '
+)
+
+criaCartao(
+    'Tipo Planta',
+    'Qual as vantagens e fraquezas do tipo planta?',
+    'Possuem vantagens contra água, terra e pedra, mas possuem desvantagens contra fogo, gelo, inseto, veneno e voador'
+)
+
+criaCartao(
+    'Tipo Metal',
+    'Qual as vantagens e fraquezas do tipo metal?',
+    'Possuem vantagens contra fada, pedra e gelo, mas possuem desvantagens contra fogo, lutador e terra'
+)
+
+criaCartao(
+    'Tipo Psíquico',
+    'Qual as vantagens e fraquezas do tipo psíquico?',
+    'Possuem vantagens contra ludador e veneno, mas possuem desvantagens contra fantasma, sombrio e inseto'
+)
+
+criaCartao(
+    'Tipo Sombrio',
+    'Qual as vantagens e fraquezas do tipo sombrio?',
+    'Possuem vantagens contra psíquico e fantasma, mas possuem desvantagens contra fada, lutador e inseto'
+)
+
+criaCartao(
+    'Tipo Lutador',
+    'Qual as vantagens e fraquezas do tipo lutador?',
+    'Possuem vantagens contra normal, pedra, metal e sombrio , mas possuem desvantagens contra psíquico, voador e fada'
+)
+
+criaCartao(
+    'Tipo Dragão',
+    'Qual as vantagens e fraquezas do tipo dragão?',
+    'Possuem vantagens apenas contra dragão, mas possuem desvantagens contra gelo, fada e dragão'
+)
+
+criaCartao(
+    'Tipo Fada',
+    'Qual as vantagens e fraquezas do tipo fada?',
+    'Possuem vantagens contra dragão, lutador e sombrio, mas possuem desvantagens contra metal e veneno'
+)
